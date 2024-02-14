f5f793d28bfcff40d22cb09a1095d80ee791e0ef
alex02r <alex.giochi02@gmail.com>
2024-02-14 14:56:53 +0100

    [UPGRADE] Ciclate tutte il informazioni del db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,20 @@
 
 @section('content')
     <div class="container py-5">
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-12">
                 <h1>testo prova</h1>
+            </div>
+            <div class="col-8">
+                @foreach ($movies as $movie)
+                    <ul>
+                        <li>Titolo: {{ $movie->title}}</li>
+                        <li>Titolo originale:{{ $movie->original_title}}</li>
+                        <li>Nazionalità:{{ $movie->nationality}}</li>
+                        <li>Data di uscita:{{ $movie->date}}</li>
+                        <li>Valutazione:{{ $movie->vote}}</li>
+                    </ul>
+                @endforeach
             </div>
         </div>
     </div>
